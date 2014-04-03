e2c773c12a3bb471f3ec913d42a6415c2f0f0576
xiaoqiangkx <xiaoqiangkx@gmail.com>
2014-04-03 09:58:51 +0800

    test passed for insert vertex

--- tests/webgraph_tests.c
+++ tests/webgraph_tests.c
@@ -1,4 +1,6 @@
 #include <webgraph.h>
+#include <hash.h>
+#include <string.h>
 #include "minunit.h"
 
 webg_t *webg = NULL;
@@ -34,18 +36,32 @@ char* test_insert_vertex() {
     insert_vertex(webg, s1);
     mu_assert(size(webg) == 1, "size of webg is not 1");
     mu_assert(webg->sm_vertex_dict[0].url_num_node->num == 0, "the num of s1 is 0"); 
+  //  debug("s1 is %s", s1);
+  //  debug("data in sm is %s", webg->sm_vertex_dict[0].url_num_node->url);
+    mu_assert(!strncmp(s1, webg->sm_vertex_dict[0].url_num_node->url, strlen(s1)), "s1 is changed");
+    mu_assert(webg->sm_vertex_dict[0].next == NULL, "next of webg[0] is not NULL");
+    mu_assert(!strncmp(webg->url_dict[sax_hash(s1) % HASH_MAX].next->url, s1, strlen(s1)), "success to hash s1");
 
     insert_vertex(webg, s2);
     mu_assert(size(webg) == 2, "size of webg is not 2");
     mu_assert(webg->sm_vertex_dict[1].url_num_node->num == 1, "the num of s1 is 1"); 
+    mu_assert(!strncmp(s2, webg->sm_vertex_dict[1].url_num_node->url, strlen(s2)), "s2 is changed");
+    mu_assert(webg->sm_vertex_dict[1].next == NULL, "next of webg[0] is not NULL");
+    mu_assert(!strncmp(webg->url_dict[sax_hash(s2) % HASH_MAX].next->url, s2, strlen(s2)), "success to hash s2");
 
     insert_vertex(webg, s3);
     mu_assert(size(webg) == 3, "size of webg is not 3");
     mu_assert(webg->sm_vertex_dict[2].url_num_node->num == 2, "the num of s1 is 2"); 
+    mu_assert(!strncmp(s3, webg->sm_vertex_dict[2].url_num_node->url, strlen(s3)), "s3 is changed");
+    mu_assert(webg->sm_vertex_dict[2].next == NULL, "next of webg[0] is not NULL");
+    mu_assert(!strncmp(webg->url_dict[sax_hash(s3) % HASH_MAX].next->url, s3, strlen(s3)), "success to hash s3");
 
     insert_vertex(webg, s4);
     mu_assert(size(webg) == 4, "size of webg is not 4");
     mu_assert(webg->sm_vertex_dict[3].url_num_node->num == 3, "the num of s1 is 3"); 
+    mu_assert(!strncmp(s4, webg->sm_vertex_dict[3].url_num_node->url, strlen(s4)), "s4 is changed");
+    mu_assert(webg->sm_vertex_dict[3].next == NULL, "next of webg[0] is not NULL");
+    mu_assert(!strncmp(webg->url_dict[sax_hash(s4) % HASH_MAX].next->url, s4, strlen(s4)), "success to hash s4");
 
     return NULL;
 }
@@ -74,13 +90,13 @@ char *all_tests() {
     mu_suite_start();
 
     mu_run_test(test_init_webg);
-    mu_run_test(test_destroy_webg);
     mu_run_test(test_insert_vertex);
     mu_run_test(test_insert_edge);
     mu_run_test(test_insert_vertex_edge);
     mu_run_test(test_set_num);
     mu_run_test(test_get_num);
     mu_run_test(test_set_get_num);
+    mu_run_test(test_destroy_webg);
 
     return NULL;
 }
